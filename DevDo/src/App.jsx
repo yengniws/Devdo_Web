a23ecf845fb3faa742b0380920a06eca7fda854e
yeeun park <145003970+yengniws@users.noreply.github.com>
2025-07-12 23:11:38 +0900

    Feat : Development environment setup

---
+++ DevDo/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+   return (
+      <>
+         <div className="bg-navy">
+            <h1 className="text-neon-green font-roboto-mono">Hello</h1>
+         </div>
+      </>
+   );
+}
+
+export default App;
---
+++ DevDo/src/main.jsx
@@ -0,0 +1,5 @@
+import { createRoot } from "react-dom/client";
+import "../index.css"; // 테일윈드 임포트
+import App from "./App.jsx";
+
+createRoot(document.getElementById("root")).render(<App />);
