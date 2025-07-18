b8a4200f7e303ed14ac48a202696c81b1b760d54
rryuuni <fkdbs1122@naver.com>
2025-07-18 17:10:45 +0900

    Feat/#10 : CommunityList Search on going

--- DevDo/src/pages/Community/CommunityList.jsx
+++ DevDo/src/pages/Community/CommunityList.jsx
@@ -1,14 +1,24 @@
-import { IoIosSearch } from 'react-icons/io';
 import { IoEyeOutline } from 'react-icons/io5';
 import DummyCommunity from '../../constants/DummyCommunity';
 import { useState } from 'react';
 import Pagination from '../../components/Pagination';
 import { Link } from 'react-router-dom';
+import CommunityListSearch from '../../components/CommunityListSearch';
 
 const CommunityList = ({ community = DummyCommunity }) => {
    const [items] = useState(community);
    const [currentPage, setCurrentPage] = useState(1);
    const itemsPerPage = 6;
+
+   const [searchTerm, setSearchTerm] = useState('');
+
+   const filteredData = community.filter((item) =>
+      item.title.toLowerCase().includes(searchTerm.toLowerCase()),
+   );
+
+   const onSearch = (term) => {
+      setSearchTerm(term);
+   };
 
    const indexOfLastItem = currentPage * itemsPerPage;
    const indexOfFirstItem = indexOfLastItem - itemsPerPage;
@@ -21,45 +31,42 @@ const CommunityList = ({ community = DummyCommunity }) => {
             <div className="w-[100%] my-[1%] border-[1px] border-navy"></div>
          </div>
          <div className="w-[100%] h-[67px] relative flex bg-gray rounded-2xl text-2xl mt-4">
-            <input
-               type="text"
-               placeholder="제목으로 글 찾기"
-               className="w-[100%] pl-10 placeholder:text-2xl font-light"
-            />
-            <button className="absolute top-[6px] bottom-[6px] right-[20px] rounded-[15px] cursor-pointer">
-               <IoIosSearch className="text-5xl" />
-            </button>
+            <CommunityListSearch searchTerm={searchTerm} onSearch={onSearch} />
          </div>
 
-         <div className="flex flex-col mt-6 gap-2 font-pretendard p-3">
-            {currentItems.map((community) => (
-               <div key={community.id}>
-                  <Link to={`/community/${community.id}`}>
-                     <div className="w-ful p-4 flex justify-between items-center pb-2 pt-0">
-                        <div className="flex flex-row ">
-                           <div className="text-2xl font-normal mr-3">
-                              {community.title}
+         {filteredData && filteredData.length > 0 ? (
+            <div className="flex flex-col mt-6 gap-2 font-pretendard p-3">
+               {currentItems.map((community) => (
+                  <div key={community.id}>
+                     <Link to={`/community/${community.id}`}>
+                        <div className="w-ful p-4 flex justify-between items-center pb-2 pt-0">
+                           <div className="flex flex-row ">
+                              <div className="text-2xl font-normal mr-3">
+                                 {community.title}
+                              </div>
+                              <div className="text-xl text-neon-green font-normal">
+                                 [{community.comment}]
+                              </div>
                            </div>
-                           <div className="text-xl text-neon-green font-normal">
-                              [{community.comment}]
-                           </div>
-                        </div>
-                        <div className="flex flex-row">
-                           <div className="text-xl font-light mr-5">
-                              {community.date}
-                           </div>
+                           <div className="flex flex-row">
+                              <div className="text-xl font-light mr-5">
+                                 {community.date}
+                              </div>
 
-                           <div className="flex items-center gap-1 font-light text-xl">
-                              <IoEyeOutline />
-                              <span>{community.view}</span>
+                              <div className="flex items-center gap-1 font-light text-xl">
+                                 <IoEyeOutline />
+                                 <span>{community.view}</span>
+                              </div>
                            </div>
                         </div>
-                     </div>
-                  </Link>
-                  <div className="w-[100%] my-[1%] border-[1px] border-gray "></div>
-               </div>
-            ))}
-         </div>
+                     </Link>
+                     <div className="w-[100%] my-[1%] border-[1px] border-gray "></div>
+                  </div>
+               ))}
+            </div>
+         ) : (
+            <span className="">검색 결과가 없습니다.</span>
+         )}
 
          <div className="flex justify-end mr-4 ">
             <Link to="/community/write">
