ef9cb2ef93b3d54a982d8966c017ad27331fa6b2
yeeun park <145003970+yengniws@users.noreply.github.com>
2025-07-20 17:53:15 +0900

    Feat : Added routing #13

--- DevDo/src/App.jsx
+++ DevDo/src/App.jsx
@@ -1,5 +1,6 @@
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
+import RoadmapDetail from './pages/RoadmapDetail';
 import Layout from './components/Layout';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -10,6 +11,9 @@ function App() {
             <Route path="/login" element={<Login />} />
             <Route element={<Layout />}>
                <Route path="/" element={<Dashboard />} />
+               {/* <Route path="/roadmap/:id" element={<RoadmapDetail />} /> */}
+               {/* 로드맵 개발 완료 후  위 코드(:id)로 수정 */}
+               <Route path="/roadmap/detail" element={<RoadmapDetail />} />
             </Route>
          </Routes>
       </>
---
+++ DevDo/src/pages/RoadmapDetail.jsx
@@ -0,0 +1,4 @@
+const RoadmapDetail = () => {
+   return <div></div>;
+};
+export default RoadmapDetail;
